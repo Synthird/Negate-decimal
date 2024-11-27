f43274a2a69fa50fc8bd7523e6e271aed7ae2e8a
Synthird <105394020+Synthird@users.noreply.github.com>
2024-11-26 21:06:23 -0800

    Showed more numbers being negated

--- src/Example.java
+++ src/Example.java
@@ -4,9 +4,23 @@ public class Example {
     private static NegateDecimal negateDecimal = new NegateDecimal();
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Negated double of 156.415:");
-        System.out.println(negateDecimal.negateDouble(156.415));
-        System.out.println("Negated float of 345.4f:");
-        System.out.println(negateDecimal.negateFloat(345.4f));
+        System.out.println("---- DOUBLES ----");
+        revealNegatedDouble(156.415);
+        revealNegatedDouble(1);
+        revealNegatedDouble(0);
+        System.out.println("---- FLOAT ----");
+        revealNegatedFloat(345.4f);
+        revealNegatedFloat(1);
+        revealNegatedFloat(0);
+    }
+
+    private static void revealNegatedDouble(double number) {
+        System.out.println(String.format("Negated double of %s:", number));
+        System.out.println(negateDecimal.negateDouble(number));
+    }
+
+    private static void revealNegatedFloat(float number) {
+        System.out.println(String.format("Negated float of %sf:", number));
+        System.out.println(negateDecimal.negateFloat(number));
     }
 }
