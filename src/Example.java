b20a39c5777fec553dd973441644065c6a8b331c
Synthird <105394020+Synthird@users.noreply.github.com>
2024-11-26 20:57:20 -0800

    Formatted the example code and added a period at the end of the comment

--- src/Example.java
+++ src/Example.java
@@ -1,6 +1,8 @@
 public class Example {
-    // This file is an example of using the negateDecimal methods
+    // This file is an example of using the negateDecimal methods.
+
     private static NegateDecimal negateDecimal = new NegateDecimal();
+
     public static void main(String[] args) throws Exception {
         System.out.println("Negated double of 156.415:");
         System.out.println(negateDecimal.negateDouble(156.415));
