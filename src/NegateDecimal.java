632c57beaf6d171a139c3e33f753c050725cb3cc
Synthird <105394020+Synthird@users.noreply.github.com>
2024-12-10 00:38:21 -0800

    Javadocs are added above the methods

--- src/NegateDecimal.java
+++ src/NegateDecimal.java
@@ -1,8 +1,16 @@
 public class NegateDecimal {
+    /**
+     * @param decimalFloat
+     * @return negated decimal of a float
+     */
     public float negateFloat(float decimalFloat) {
         return decimalFloat - decimalFloat - decimalFloat;
     }
 
+    /**
+     * @param decimalDouble
+     * @return negated decimal of a double
+     */
     public double negateDouble(double decimalDouble) {
         return decimalDouble - decimalDouble - decimalDouble;
     }
