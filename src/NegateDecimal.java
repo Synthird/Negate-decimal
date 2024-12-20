cd14881fa746b91c50ea443ce348210a7f0dcf63
Synthird <105394020+Synthird@users.noreply.github.com>
2024-12-19 21:48:45 -0800

    Replace spaces with tabs when code is indented (Done with javadocs)

--- src/NegateDecimal.java
+++ src/NegateDecimal.java
@@ -1,17 +1,17 @@
 public class NegateDecimal {
-    /**
-     * @param decimalFloat
-     * @return Returns a negative version of a positive double and vice versa.
-     */
-    public float negateFloat(float decimalFloat) {
+	/**
+	 * @param decimalFloat
+	 * @return Returns a negative version of a positive double and vice versa.
+	 */
+	public float negateFloat(float decimalFloat) {
 		return decimalFloat - decimalFloat - decimalFloat;
-    }
+	}
 
-    /**
-     * @param decimalDouble
-     * @return Returns a negative version of a positive float and vice versa.
-     */
-    public double negateDouble(double decimalDouble) {
+	/**
+	 * @param decimalDouble
+	 * @return Returns a negative version of a positive float and vice versa.
+	 */
+	public double negateDouble(double decimalDouble) {
 		return decimalDouble - decimalDouble - decimalDouble;
-    }
+	}
 }
