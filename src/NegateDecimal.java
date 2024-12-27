82e877a229ce3239754c473bb813efdd500cf10a
Synthird <105394020+Synthird@users.noreply.github.com>
2024-12-26 22:44:55 -0800

    Moved the negateDouble method above the negateFloat method to match the alignment in the README

--- src/NegateDecimal.java
+++ src/NegateDecimal.java
@@ -1,17 +1,17 @@
 public class NegateDecimal {
 	/**
-	 * @param decimalFloat
-	 * @return Returns a negative version of a positive double and vice versa.
-	 */
-	public float negateFloat(float decimalFloat) {
-		return decimalFloat * -1.0f;
-	}
-
-	/**
 	 * @param decimalDouble
 	 * @return Returns a negative version of a positive float and vice versa.
 	 */
 	public double negateDouble(double decimalDouble) {
 		return decimalDouble * -1.0;
+	}
+
+	/**
+	 * @param decimalFloat
+	 * @return Returns a negative version of a positive double and vice versa.
+	 */
+	public float negateFloat(float decimalFloat) {
+		return decimalFloat * -1.0f;
 	}
 }
