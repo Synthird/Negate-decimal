6e195761a7888709ad3f0df5026c12daaa8b2aab
Synthird <105394020+Synthird@users.noreply.github.com>
2025-02-13 15:41:34 -0800

    Shortened javadoc method descriptions

--- src/NegateDecimal.java
+++ src/NegateDecimal.java
@@ -1,7 +1,7 @@
 public class NegateDecimal {
 	/**
 	 * @param decimalDouble
-	 * @return Returns a negative version of a positive float and vice versa.
+	 * @return A negative version of a positive float and vice versa.
 	 */
 	public double negateDouble(double decimalDouble) {
 		return decimalDouble * -1.0;
@@ -9,7 +9,7 @@ public class NegateDecimal {
 
 	/**
 	 * @param decimalFloat
-	 * @return Returns a negative version of a positive double and vice versa.
+	 * @return A negative version of a positive double and vice versa.
 	 */
 	public float negateFloat(float decimalFloat) {
 		return decimalFloat * -1.0f;
@@ -17,7 +17,7 @@ public class NegateDecimal {
 
 	/**
 	 * @param decimal
-	 * @return Returns a negative version of a positive double and vice versa.
+	 * @return A negative version of a positive double and vice versa.
 	 */
 	public double negate(double decimal) {
 		return negateDouble(decimal);
@@ -25,7 +25,7 @@ public class NegateDecimal {
 
 	/**
 	 * @param decimal
-	 * @return Returns a negative version of a positive float and vice versa.
+	 * @return A negative version of a positive float and vice versa.
 	 */
 	public float negate(float decimal) {
 		return negateFloat(decimal);
